d73ae51446369bb131d97432fece3208da383a7e
DhruvGupta0801 <85752740+DhruvGupta0801@users.noreply.github.com>
2025-03-12 14:11:20 -0400

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -4,4 +4,4 @@ def test_square_area_positive():
     assert square_area(2) == 4
 
 def test_square_area_student_id():
-    assert square_area(6) == 40  # 40 is wrong; it should be 36
+    assert square_area(6) == 36
